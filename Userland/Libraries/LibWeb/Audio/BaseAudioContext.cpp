60de34b48f9953bc247618cce8f9693a25de515d
serenitydev <ben.d.abraham@gmail.com>
2022-02-10 16:15:10 -0500

    LibWeb: AudioContext stub implementation

---
+++ Userland/Libraries/LibWeb/Audio/BaseAudioContext.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022, Ben Abraham
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/String.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/Bindings/BaseAudioContextWrapper.h>
+#include <LibWeb/Audio/BaseAudioContext.h>
+
+namespace Web::Audio {
+
+// https://webaudio.github.io/web-audio-api/#baseaudiocontext
+
+BaseAudioContext::BaseAudioContext()
+{
+}
+
+JS::Object* BaseAudioContext::create_wrapper(JS::GlobalObject& global_object)
+{
+    return wrap(global_object, *this);
+}
+
+}
---
+++ Userland/Libraries/LibWeb/Audio/BaseAudioContext.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022, Ben Abraham
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/RefCounted.h>
+#include <AK/URLParser.h>
+#include <LibWeb/Bindings/BaseAudioContextWrapper.h>
+#include <LibWeb/Bindings/WindowObject.h>
+#include <LibWeb/Bindings/Wrappable.h>
+#include <LibWeb/DOM/Document.h>
+#include <LibWeb/DOM/EventTarget.h>
+#include <LibWeb/DOM/ExceptionOr.h>
+#include <LibWeb/Forward.h>
+
+namespace Web::Audio {
+
+// https://html.spec.whatwg.org/multipage/workers.html#dedicated-workers-and-the-worker-interface
+class BaseAudioContext
+    : public RefCounted<Audio::BaseAudioContext>
+    , public Weakable<BaseAudioContext>
+    , public DOM::EventTarget
+    , public Bindings::Wrappable {
+public:
+    using WrapperType = Bindings::BaseAudioContextWrapper;
+
+    using RefCounted::ref;
+    using RefCounted::unref;
+
+    static NonnullRefPtr<Audio::BaseAudioContext> create()
+    {
+        return adopt_ref(*new Audio::BaseAudioContext());
+    }
+    static NonnullRefPtr<Audio::BaseAudioContext> create_with_global_object(Bindings::WindowObject&)
+    {
+        return Audio::BaseAudioContext::create();
+    }
+
+    virtual ~BaseAudioContext() = default;
+
+    // ^EventTarget
+    virtual void ref_event_target() override { ref(); }
+    virtual void unref_event_target() override { unref(); }
+    virtual JS::Object* create_wrapper(JS::GlobalObject&) override;
+
+protected:
+    BaseAudioContext();
+
+private:
+};
+
+} // namespace Web::Audio
+
+namespace Web::Bindings {
+
+BaseAudioContextWrapper* wrap(JS::GlobalObject&, Audio::BaseAudioContext&);
+
+}
